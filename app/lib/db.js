0c287128524edfc223e377232758845fbdc96e81
Daveralphy <raphaeldaveal@gmail.com>
2026-01-23 11:00:57 +0100

    Fix SSL certificate issue - explicitly allow self-signed certs

--- app/lib/db.js
+++ app/lib/db.js
@@ -5,7 +5,10 @@ let pool;
 if (!global.pool) {
   global.pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: {
+      rejectUnauthorized: false,
+      sslmode: "require",
+    },
   });
 }
 pool = global.pool;
