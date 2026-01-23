7d26b1a739d0a37c92e32683eaa754e0d9d566e6
Daveralphy <raphaeldaveal@gmail.com>
2026-01-23 23:17:49 +0100

    webpage

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,8 @@ export const metadata = {
       { url: '/images/favicon.ico', sizes: 'any' },
       { url: '/images/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
       { url: '/images/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+      { url: '/images/android-chrome-192x192.png', type: 'image/png', sizes: '192x192' },
+      { url: '/images/android-chrome-512x512.png', type: 'image/png', sizes: '512x512' },
     ],
     apple: [
       { url: '/images/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
