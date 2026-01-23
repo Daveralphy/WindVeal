e6b446a5242849dab4e42f185d62af90bc30fe8e
Daveralphy <raphaeldaveal@gmail.com>
2026-01-23 18:29:15 +0100

    favicon

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,17 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'WindVeal',
   description: 'Hybrid AI Assistant',
+  icons: {
+    icon: [
+      { url: '/images/favicon.ico', sizes: 'any' },
+      { url: '/images/favicon-32x32.png', type: 'image/png', sizes: '32x32' },
+      { url: '/images/favicon-16x16.png', type: 'image/png', sizes: '16x16' },
+    ],
+    apple: [
+      { url: '/images/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
+    ],
+  },
+  manifest: '/images/site.webmanifest',
 }
 
 export default function RootLayout({ children }) {
